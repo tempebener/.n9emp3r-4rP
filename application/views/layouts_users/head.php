ee3f3a9020067a3d61ed736ead06e12393cf9f06
tempebener <bayoewa@gmail.com>
2023-05-03 16:16:29 +0700

    Login Selesai
    . PR di Register

--- application/views/layouts_users/head.php
+++ application/views/layouts_users/head.php
@@ -16,4 +16,14 @@
     <!-- main css -->
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/bulkapp/css/style.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/bulkapp/css/responsive.css">
+
+    <!-- AdminLTE 3.2.0 -->
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/adminlte3/plugins/fontawesome-free/css/all.min.css">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/adminlte3/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/adminlte3/dist/css/adminlte.min.css">
 </head>
