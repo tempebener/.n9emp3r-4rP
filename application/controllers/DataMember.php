7c91075b0b053dcce310e2c81bbb24bc9b0ed366
tempebener <bayoewa@gmail.com>
2023-05-10 16:43:41 +0700

    Update DataMember.php

--- application/controllers/DataMember.php
+++ application/controllers/DataMember.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class DataMember extends CI_Controller {
+class Datamember extends CI_Controller {
 
 	public function __construct() 
 	{
