cda29496b9793184b82c6228d8c35f3d39f7b13e
tempebener <bayoewa@gmail.com>
2023-05-10 16:52:02 +0700

    Update SettingsPasswordAdmin.php

--- application/controllers/SettingsPasswordAdmin.php
+++ application/controllers/SettingsPasswordAdmin.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class SettingsPasswordAdmin extends CI_Controller {
+class Settingspasswordadmin extends CI_Controller {
 
 	public function __construct() 
   	{
